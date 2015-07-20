e8875f58d89e15897f2ba8b0d9b2a206ee1cb974
Isaac Archer <archeri@cat.pdx.edu>
2015-07-19 19:49:00 -0700

    update

--- husk.rb
+++ husk.rb
@@ -158,7 +158,7 @@ def join(message)
 
       tokens = message.message.split(" ")
 
-      if !tokens[1].to_s.match("/^#/") then bot.notice(message.nick, "#{tokens[1] is an invalid channel name}"); return; end
+      if !tokens[1].to_s.match("/^#/") then bot.notice(message.nick, "#{tokens[1]} is an invalid channel name"); return; end
 
       bot.join("#{tokens[1]}")
       # bot.notice("#{tokens[1]}", "hello: for help with this bot use `help")
