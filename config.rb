e26b47ca3f450ce77d4cd9b02aec7b9a55fac9ee
Isaac Archer <archeri@cat.pdx.edu>
2015-07-19 19:56:21 -0700

    update

--- config.rb
+++ config.rb
@@ -14,71 +14,90 @@
 
 module Config_var
 
-      # irc server address
-      @network = "irc.freenode.net"
+      attr_accessor :network
+      attr_accessor :port
+      attr_accessor :use_ssl
+      attr_accessor :pass
+      attr_accessor :nick
+      attr_accessor :username
+      attr_accessor :realname
+      attr_accessor :use_pass
+      attr_accessor :nickserv_pass
+      attr_accessor :dev_admin
+      attr_accessor :channels
+      attr_accessor :admins
+      attr_accessor :ignore_list
+      attr_accessor :plugins_list
+      attr_accessor :plugin_dir
+      attr_accessor :command_prefix
+      
+      def initialize
+            # irc server address
+            @network = "irc.freenode.net"
 
-      # irc server port
-      @port = 6697
+            # irc server port
+            @port = 6697
 
-      # whether the irc bot should use ssl or not
-      @use_ssl = true
+            # whether the irc bot should use ssl or not
+            @use_ssl = true
 
-      # irc server password
-      @pass = ""
+            # irc server password
+            @pass = ""
 
-      # irc bot nickname
-      @nick = "husk"
+            # irc bot nickname
+            @nick = "husk"
 
-      # irc bot user name
-      @username = "rircbot"
+            # irc bot user name
+            @username = "rircbot"
 
-      # irc bot real name
-      @realname = "rircbot"
+            # irc bot real name
+            @realname = "rircbot"
 
-      # whether the irc bot should use a password for the irc server or not
-      @use_pass = false
+            # whether the irc bot should use a password for the irc server or not
+            @use_pass = false
 
-      # irc bot nickserv password
-      @nickserv_pass = ""
+            # irc bot nickserv password
+            @nickserv_pass = ""
 
-      # nick for core control reloading
-      @dev_admin = "YOURNICK"
+            # nick for core control reloading
+            @dev_admin = "YOURNICK"
 
-      # channels for the irc bot to join
-      @channels = ["#YOURCHANNEL"]
+            # channels for the irc bot to join
+            @channels = ["#YOURCHANNEL"]
 
-      # list of admins by nick
-      @admins = ["YOURNICK", "YOUROTHERNICK"]
+            # list of admins by nick
+            @admins = ["YOURNICK", "YOUROTHERNICK"]
 
-      # list of ignored nicks
-      @ignore_list = []
+            # list of ignored nicks
+            @ignore_list = []
 
-      # list of plugins to load
-      @plugins_list = ["cat.rb", "youtube.rb"]
+            # list of plugins to load
+            @plugins_list = ["cat.rb", "youtube.rb"]
 
-      # file path to plugin directory
-      @plugin_dir = "./plugins"
+            # file path to plugin directory
+            @plugin_dir = "./plugins"
 
-      # regexes used to call command functions
-      # unless you are going to modify commands.rb do not touch this
-      @command_prefix = [
-                        /^`info$/,
-                        /^`join ##?/,
-                        /^`part$/,
-                        /^`plsgo$/,
-                        /^`help /,
-                        /^`help$/,
-                        /^`load /,
-                        /^`unload /,
-                        /^`reload /,
-                        /^`list$/,
-                        /^`list channels$/,
-                        /^`list admins$/
-                       ]
+            # regexes used to call command functions
+            # unless you are going to modify commands.rb do not touch this
+            @command_prefix = [
+                              /^`info$/,
+                              /^`join ##?/,
+                              /^`part$/,
+                              /^`plsgo$/,
+                              /^`help /,
+                              /^`help$/,
+                              /^`load /,
+                              /^`unload /,
+                              /^`reload /,
+                              /^`list$/,
+                              /^`list channels$/,
+                              /^`list admins$/
+                             ]
 
-                       #/^`ignore /,
-                       #/^`unignore /,
-                       #/^`list ignore/,
-                       #/^`msg /,
-                       #/^`act /,
+                             #/^`ignore /,
+                             #/^`unignore /,
+                             #/^`list ignore/,
+                             #/^`msg /,
+                             #/^`act /,
+      end
 end
